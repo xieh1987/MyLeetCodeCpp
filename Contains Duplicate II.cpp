7305bb1fd8583686b27ac5a8d9f1e09c0a1143c5
He Xie <xieh1987@users.noreply.github.com>
2016-04-11 11:37:20 -0400

    Create Contains Duplicate II.cpp

---
+++ Contains Duplicate II.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> mymap;
+        for (int i=0;i<nums.size();++i) {
+            if (mymap.find(nums[i])!=mymap.end()&&i-mymap[nums[i]]<=k) return true;
+            else mymap[nums[i]] = i;
+        }
+        return false;
+    }
+};
