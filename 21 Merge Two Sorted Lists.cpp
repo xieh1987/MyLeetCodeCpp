f5ed66b1628c9663fe3d7b10b021861ed3a4aa2d
He Xie <xieh1987@users.noreply.github.com>
2018-06-16 20:53:43 -0400

    improved using recursion

--- 21 Merge Two Sorted Lists.cpp
+++ 21 Merge Two Sorted Lists.cpp
@@ -9,23 +9,15 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* dumphead = new ListNode(-1);
-        ListNode* curr = dumphead;
-        while (l1!=NULL&&l2!=NULL) {
-            if (l1->val<l2->val) {
-                curr->next = l1;
-                l1 = l1->next;
-            }
-            else {
-                curr->next = l2;
-                l2 = l2->next;
-            }
-            curr = curr->next;
+        if(l1==NULL) return l2;
+        if(l2==NULL) return l1;
+        ListNode* temp;
+        if(l1->val>l2->val){
+            temp = l2;
+            l2 = l1;
+            l1 = temp;
         }
-        if (l1==NULL) curr->next = l2;
-        else curr->next = l1;
-        ListNode* res = dumphead->next;
-        delete dumphead;
-        return res;
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
     }
 };
