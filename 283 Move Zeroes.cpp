186312e2f5204b9f1fa1242ea0457fc82cf87e64
He Xie <xieh1987@users.noreply.github.com>
2018-06-19 20:57:54 -0400

    Update and rename Move Zeroes.cpp to 283 Move Zeroes.cpp
    double pointers. Ensure the numbers between the two pointers are all 0's.

--- 283 Move Zeroes.cpp
+++ 283 Move Zeroes.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int p_0 = 0;
+        for(int i=0;i<nums.size();++i)
+            if(nums[i])
+                swap(nums[i], nums[p_0++]);
+    }
+};
+
+/****************************************************
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
         int idzero = 0;
         for (int index=0;index<nums.size();++index) {
             if (nums[index]!=0) {
@@ -12,3 +23,4 @@ public:
         }
     }
 };
+***************************************************/
