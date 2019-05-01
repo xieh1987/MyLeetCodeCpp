5bf09e0cb2a1fc282603b62d9c5ba4c04e3c90af
He Xie <h.xie1987@gmail.com>
2019-05-01 15:00:52 -0700

    Update 78 Subsets.cpp

--- 78 Subsets.cpp
+++ 78 Subsets.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
         vector<int> elem;
         vector<vector<int>> res;
         res.push_back(elem);
-        helper(nums, res, elem, 0, n);
+        helper(nums, res, elem, 0);
         return res;
     };
     
-    void helper(vector<int>& nums, vector<vector<int>>& res, vector<int>& elem, int start, int length)
+    void helper(vector<int>& nums, vector<vector<int>>& res, vector<int>& elem, int start)
     {
-        if(start==length) return;
+        if(start==nums.size()) return;
         elem.push_back(nums[start]);
         res.push_back(elem);
-        helper(nums, res, elem, start+1, length);
+        helper(nums, res, elem, start+1);
         elem.pop_back();
-        helper(nums, res, elem, start+1, length);
+        helper(nums, res, elem, start+1);
     }
 };
