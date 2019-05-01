f6892a9f00ccdec7f391afad0109c18ae7201143
He Xie <h.xie1987@gmail.com>
2019-05-01 11:06:23 -0700

    Create 73 Set Matrix Zeroes.cpp

---
+++ 73 Set Matrix Zeroes.cpp
@@ -0,0 +1,61 @@
+//O(m+n) Space
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        set<int> row, col;
+        for(int i=0;i<m;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row.insert(i);
+                    col.insert(j);
+                }
+            }
+        }
+        for(int i=0;i<m;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                if(row.find(i)!=row.end()||col.find(j)!=col.end())
+                    matrix[i][j] = 0;
+            }
+        }
+    }
+};
+
+/*O(1) Space
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        bool row = false, col = false;
+        for(int i=0;i<m;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                if(matrix[i][j]==0)
+                {
+                    if(i==0) row = true;
+                    if(j==0) col = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for(int i=1;i<m;++i)
+        {
+            for(int j=1;j<n;++j)
+            {
+                if(!matrix[i][0]||!matrix[0][j]) matrix[i][j] = 0;
+            }
+        }
+        if(row)
+            for(int j=0;j<n;++j) matrix[0][j] = 0;
+        if(col)
+            for(int i=0;i<m;++i) matrix[i][0] = 0;
+    }
+};
+*/
