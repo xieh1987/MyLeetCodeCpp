5fdefbeb703ba4a1d68bf10806f8ef396aa5003d
He Xie <h.xie1987@gmail.com>
2019-05-02 09:55:06 -0700

    Update 14 Longest Common Prefix.cpp

--- 14 Longest Common Prefix.cpp
+++ 14 Longest Common Prefix.cpp
@@ -12,3 +12,26 @@ public:
         return res;
     }
 };
+
+/*Old implementation
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size()==0) return "";
+        string common = strs[0];
+        int count = common.size();
+        for (int i=1;i<strs.size();++i) {
+            for (int j=0;j<count;++j) {
+                if (strs[i][j]!=common[j]) {
+                    common = common.substr(0,j);
+                    count = j;
+                }
+                if (count==0) {
+                    return "";
+                }
+            }
+        }
+        return common;
+    }
+};
+*/
