c67993cdcf6f4facad6ea0e665b29d4e52b1ed1a
He Xie <h.xie1987@gmail.com>
2019-05-02 10:07:51 -0700

    Update 169 Majority Element.cpp

--- 169 Majority Element.cpp
+++ 169 Majority Element.cpp
@@ -15,3 +15,20 @@ public:
         return res;
     }
 };
+
+/*Old implementation Using Map to Count All Elements
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> mymap;
+        for(vector<int>::iterator it=nums.begin();it!=nums.end();++it){
+            if(mymap.find(*it)==mymap.end())
+            mymap[*it] = 1;
+            else
+            mymap[*it] += 1;
+            if(mymap[*it]>nums.size()/2)
+            return *it;
+        }
+    }
+};
+*/
