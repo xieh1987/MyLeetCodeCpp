a1348ba9eb15ce44769f7012d7c7ab915461ec34
He Xie <h.xie1987@gmail.com>
2019-05-03 13:52:17 -0700

    Update 0046 Permutations.cpp

--- 0046 Permutations.cpp
+++ 0046 Permutations.cpp
@@ -15,3 +15,27 @@ public:
         }
     }
 };
+
+/*Alternative Solution: no change of the original nums, but record visited numbers.
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> vec, visited(nums.size(), 0);
+        helper(res, nums, vec, visited, 0);
+        return res;
+    }
+    
+    void helper(vector<vector<int>>& res, vector<int>& nums, vector<int>& vec, vector<int>& visited, int len) {
+        if(len==nums.size()) res.push_back(vec);
+        for(int i=0;i<nums.size();++i) {
+            if(visited[i]) continue;
+            visited[i] = 1;
+            vec.push_back(nums[i]);
+            helper(res, nums, vec, visited, len+1);
+            vec.pop_back();
+            visited[i] = 0;
+        }
+    }
+};
+*/
