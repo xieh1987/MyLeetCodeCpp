f333c14bd06b0b4c382664d7bdfc95eed92d1502
He Xie <h.xie1987@gmail.com>
2019-05-03 16:07:05 -0700

    Update 0055 Jump Game.cpp

--- 0055 Jump Game.cpp
+++ 0055 Jump Game.cpp
@@ -13,3 +13,19 @@ public:
         return false;
     }
 };
+
+/*Simplification
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(n<2) return true;
+        int mx = 0;
+        for(int i=0;i<n-1;++i) {
+            if(i>mx) break;
+            if(nums[i] + i>mx) mx = nums[i] + i;
+        }
+        return mx >= n - 1;
+    }
+};
+*/
