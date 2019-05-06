6feb3747f6b2090f98ddf1f2be602efcc5da3da2
He Xie <h.xie1987@gmail.com>
2019-05-06 12:28:30 -0700

    Update 0096 Unique Binary Search Trees.cpp

--- 0096 Unique Binary Search Trees.cpp
+++ 0096 Unique Binary Search Trees.cpp
@@ -3,13 +3,9 @@ public:
     int numTrees(int n) {
         vector<int> res(n+1, 0);
         res[0] = res[1] = 1;
-        for(int i=2;i<=n;++i) {
-            int sum = 0;
-            for(int m=1;m<=i;++m) {
-                sum += res[m-1] * res[i-m];
-            }
-            res[i] = sum;
-        }
+        for(int i=2;i<=n;++i)
+            for(int m=1;m<=i;++m)
+                res[i] += res[m-1] * res[i-m];
         return res[n];
     }
 };
