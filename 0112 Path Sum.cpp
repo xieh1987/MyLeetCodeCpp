cf1c82866f96ff8078c37381cd417d0c267e9f00
He Xie <h.xie1987@gmail.com>
2019-05-13 11:36:12 -0700

    Update 0112 Path Sum.cpp

--- 0112 Path Sum.cpp
+++ 0112 Path Sum.cpp
@@ -16,3 +16,14 @@ public:
         return false;
     }
 };
+
+/*Just simpler:
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root) return false;
+        if(!root->left&&!root->right&&root->val==sum) return true;
+        return((root->left&&hasPathSum(root->left, sum-root->val))||(root->right&&hasPathSum(root->right, sum-root->val)));
+    }
+};
+*/
