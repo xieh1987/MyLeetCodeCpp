f85d78102793dfe8fdfb65d9a2cde1f8ca9d5588
He Xie <h.xie1987@gmail.com>
2019-06-10 16:35:40 +0800

    Create 0240 Search a 2D Matrix II.cpp

---
+++ 0240 Search a 2D Matrix II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if(m==0) return false;
+        int n = matrix[0].size();
+        int i = m-1, j = 0;
+        while(i>=0&&j<n) {
+            if(matrix[i][j]==target) return true;
+            if(matrix[i][j]>target) --i;
+            else ++j;
+        }
+        return false;
+    }
+};
