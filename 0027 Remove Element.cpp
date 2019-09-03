898d40cd750126e3d6af6da0ffe4286bd09447e3
He Xie <h.xie1987@gmail.com>
2019-09-03 10:56:31 -0700

    Update 0027 Remove Element.cpp

--- 0027 Remove Element.cpp
+++ 0027 Remove Element.cpp
@@ -1,3 +1,17 @@
+//Remove elements in-place. Note that old implementations only calculated the size but did not modify the array
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        for(int i=0;i<nums.size();++i) {
+            while(nums[i]==val&&i<nums.size()) {
+                nums.erase(nums.begin()+i);
+            }
+        }
+        return nums.size();
+    }
+};
+
+/*Count the number only
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
@@ -12,7 +26,7 @@ public:
     }
 };
 
-/*Old Implementation
+//Old Implementation
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
