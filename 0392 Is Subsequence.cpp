b96fe0879f78dc1aae4e1686788df13296d7ff88
He Xie <h.xie1987@gmail.com>
2021-06-08 22:11:40 -0700

    Create 0392 Is Subsequence.cpp

---
+++ 0392 Is Subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int ls = s.size();
+        int lt = t.size();
+        vector<vector<bool>> res (ls+1, vector<bool>(lt+1, false));
+        for(int j=0;j<=lt;++j) {
+            res[0][j] = true;
+        }
+        for(int i=1;i<=ls;++i) {
+            for(int j=i;j<=lt;++j) {
+                res[i][j] = (res[i-1][j-1] && s[i-1]==t[j-1]) || (res[i][j-1]);
+            }
+        }
+        return res[ls][lt];
+    }
+};
